710cc5f37358cb064fb1b02bd2e4414ab5139115
dunglt <letrungdung1462001@gmail.com>
2025-02-20 10:39:13 +0700

    update

--- blog/app/constants/conts.ts
+++ blog/app/constants/conts.ts
@@ -1,4 +1,4 @@
-export const CLIENT_SECRET = "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";
+// export const CLIENT_SECRET = "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";
 export const CLIENT_ID = "196827591512-bm0890su1khv4684h8ker8nvci710sf0.apps.googleusercontent.com"
 export const API_KEY = "AIzaSyBxj5dRSYc1FrvpHsG0OUSfaw3skT5Wrao"
 export const SCOPES = "https://www.googleapis.com/auth/blogger"
